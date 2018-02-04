be4f285317ecc20ac92af80d16607791913f822b
Michael Chan <mikecchan0@gmail.com>
2018-02-03 19:44:44 -0500

    first init

---
+++ src/main/java/com/teamtreehouse/reviews/Main.java
@@ -0,0 +1,19 @@
+package com.teamtreehouse.reviews;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            CSVPrinter printer = new CSVPrinter(System.out, CSVFormat.EXCEL);
+            printer.printRecord("Craig", "Dennis", 5, "Loved It!");
+            printer.printRecord("Craig", "Ram", 4, "Good!");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
